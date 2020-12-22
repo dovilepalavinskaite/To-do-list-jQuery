d4c1c4e2584f146d1b28e4018d8a7b1613e13e79
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2020-12-22 17:23:49 +0200

    Cleaner JS, new jS functions, responsive design

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
 
+var newTask = $('#new-task').val();
+$('#new-task').on("input", function(){
+	 newTask = $(this).val();
+});
 
-// Create new Task TO-DO:
+// Create new Task TO-DO with button:
 
 $('input[type=submit]').click(function(){
-	const newTask = $('#new-task').val();
 	if (newTask.length) {
-		$('ul').append('<li><div class="remove-class"><i class="fas fa-trash-alt"></i></div><span>' + newTask + '</span></li>');
+	appendNew(); 
+	$(this).val('');	
 	}
 });
+
+// Create new Task TO-DO with ENTER:
+
+$('#new-task').keypress(function (e) {
+ var key = e.which;
+ if(key == 13 && newTask.length){  // the enter key code
+  appendNew();
+  $(this).val('');
+  }
+});  
+
+// Function for creating new list:
+
+function appendNew(){
+	$('ul').append('<li><div class="remove-class"><i class="fas fa-trash-alt"></i></div><span>' + newTask + '</span></li>');
+}
+
 
 // Mark Task as done:
 
@@ -31,4 +52,8 @@ $('.remove-class').click(function(){
 
 
 });
+
+
+
+
 
