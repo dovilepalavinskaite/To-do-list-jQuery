385452123f7c32d794a50748237be31bc2e0e52a
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2020-12-22 15:35:34 +0200

    Hidden class appears when hovered. Can delete task

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,31 @@ $(document).ready(function() {
 // Create new Task TO-DO:
 
 $('input[type=submit]').click(function(){
-	var newTask = $('#new-task').val();
-	$('new-task').val("");
-	$('ul').append('<li>' + newTask + '</li>');
+	const newTask = $('#new-task').val();
+	if (newTask.length) {
+		$('ul').append('<li><div class="remove-class"><i class="fas fa-trash-alt"></i></div><span>' + newTask + '</span></li>');
+	}
+});
+
+// Mark Task as done:
+
+$('span').click(function(){
+	$(this).toggleClass('done-task');
+});
+
+//Hover mouse to add class with trash bin:
+
+$('li').hover(function(){
+	$(this).children('div').toggleClass("active");
+});
+
+// Delete task:
+
+$('.remove-class').click(function(){
+	$(this).closest('li').remove();
 });
 
 
 
-
 });
-
 
